cc4aa7c670c2a4bb1daf58485a26437fd4fbb82a
YevvK <subzeroxlegend@aol.com>
2019-06-18 16:39:49 -0400

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // Authored by ‘Robert Rua’, ‘Jeremy Halek’, ‘Gaston Alvarado’, ‘Yevgeniy Khmelnitskiy’,  ‘Anthony Uhuegbue’
 // API key
-const API_KEY = "pk.eyJ1IjoieWtobWVsIiwiYSI6ImNqd2I4cWhhMDAyNzA0OHA0bWo5NGR6NTIifQ.gOV-0w3ldzaFKZ7AjGEIxQ";
+const API_KEY = "";
